60ab71568636b1a62eba37c8f19e5e676e815748
AliCheema77 <asadullahcheema77@gmail.com>
2022-03-30 13:21:24 +0500

    Added two new models PostImage and Post and added image field in Category model

--- products/models.py
+++ products/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.core.validators import FileExtensionValidator
 
 
 class Category(models.Model):
     title = models.CharField(max_length=150)
+    image = models.FileField(upload_to="category/", validators=[FileExtensionValidator(['pdf', 'doc', 'svg'])],
+                             null=True, blank=True)
 
     class Meta:
         verbose_name = "Category"
@@ -23,4 +27,41 @@ class SubCategory(models.Model):
     def __str__(self):
         return self.title
 
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category_post")
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name="sub_category_post")
+    ad_title = models.CharField(max_length=70)
+    description = models.TextField(max_length=4096)
+    make = models.CharField(max_length=100, null=True, blank=True)
+    model = models.CharField(max_length=100, null=True, blank=True)
+    year = models.PositiveIntegerField(validators=[MinValueValidator(1900)], null=True, blank=True)
+    km_driven = models.PositiveIntegerField(null=True, blank=True)
+    fuel = models.CharField(max_length=100, null=True, blank=True)
+    registered_in = models.CharField(max_length=100, null=True, blank=True)
+    condition = models.CharField(choices=(('new', 'New'), ('used', 'Used')), max_length=10, null=True, blank=True)
+    price = models.PositiveIntegerField()
+    location = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=100, null=True, blank=True)
+    show_phone_number = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.ad_title
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
+    image = models.ImageField(upload_to='post/')
+
+    def __str__(self):
+        return self.post.ad_title
+
+    class Meta:
+        verbose_name = "Post Image"
+        verbose_name_plural = "Post Images"
 

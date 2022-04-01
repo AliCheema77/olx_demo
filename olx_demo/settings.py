c7756793ae03c0a03782b07780c4d5f0beb0a422
AliCheema77 <asadullahcheema77@gmail.com>
2022-04-01 08:55:19 +0500

    Updated

--- olx_demo/settings.py
+++ olx_demo/settings.py
@@ -96,12 +96,6 @@ WSGI_APPLICATION = 'olx_demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
